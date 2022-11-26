a076f46f040fb1d7c4e93e86789aa0cbadd98724
sathiyaamoorthi <sathi210695@gmail.com>
2022-11-26 17:37:23 +0530

    okie

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,22 @@
+package org.company;
+
+public class CompanyInfo {
+	private void companyName() {
+		// TODO Auto-generated method stub
+System.out.println("greens");
+	}
+	private void companyId() {
+		// TODO Auto-generated method stub
+System.out.println("1231");
+	}
+	private void companyAddress() {
+		// TODO Auto-generated method stub
+System.out.println("chenai");
+	}
+	public static void main(String[] args) {
+		CompanyInfo c=new CompanyInfo();
+		c.companyName();
+		c.companyId();
+		c.companyAddress();
+	}
+}
